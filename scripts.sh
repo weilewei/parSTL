23e5d62a86f012ee7ff64d8ad543e05fb8ab9cda
Weile Wei <lokwei9@gmail.com>
2023-01-23 15:57:20 -0800

    modify build scirpts

--- scripts.sh
+++ scripts.sh
@@ -2,6 +2,9 @@ module purge
 source /opt/cray/pe/cpe/22.11/restore_lmod_system_defaults.sh
 module load nvhpc/22.9
 
+export CC=/global/homes/w/wwei/src/spack/opt/spack/linux-sles15-zen3/gcc-11.2.0/gcc-11.2.0-zvvuszlajgsejuuf5o25rceihybuakbo/bin/gcc
+export CXX=/global/homes/w/wwei/src/spack/opt/spack/linux-sles15-zen3/gcc-11.2.0/gcc-11.2.0-zvvuszlajgsejuuf5o25rceihybuakbo/bin/g++
+
 mkdir -p build && cd build
 make clean
 cmake ..
